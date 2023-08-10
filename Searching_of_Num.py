62bc476352f0a5d1108a5351abef69d531e36531
MennaAbdelatif <141871133+MennaAbdelatif@users.noreply.github.com>
2023-08-10 15:27:49 +0300

    Update Searching_of_Num.py

--- Searching_of_Num.py
+++ Searching_of_Num.py
@@ -1,7 +1,7 @@
-list = (input("please enter list:"))
+mylist = list(input("please enter list:"))
 val= (input("please enter the number you are searching for:"))
 count=0
-for i in list:
+for i in mylist:
     if i == val: 
-        count += 1
+        count +=1
 print("This value repeated in the number "+str(count)+" times")
