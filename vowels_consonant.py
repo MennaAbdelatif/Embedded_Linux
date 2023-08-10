4ebcaa6e9d58dd60419bdb78fdfc3be3be5e2a54
MennaAbdelatif <141871133+MennaAbdelatif@users.noreply.github.com>
2023-08-10 13:17:54 +0300

    Update vowels_consonant.py with list and in

--- vowels_consonant.py
+++ vowels_consonant.py
@@ -1,5 +1,7 @@
 char=input("please enter the letter you want to check: ")
-if char.lower() == "a" or char.lower() == "i" or char.lower() =="o" or char.lower == "u" or char.lower =="e":
+vowels=["a","i","o","u","e"]
+
+if char.lower() in vowels:
     print("This letter is vowel")
 else:
     print("This letter is consonant")
